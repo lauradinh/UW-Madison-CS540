87ddfadfb7e516877b4638d4ef693e8ff895bc07
Laura Dinh <56850508+lauradinh@users.noreply.github.com>
2023-01-17 15:47:27 -0600

    Adding HW10

---
+++ Homework 10/Q_learning.py
@@ -0,0 +1,82 @@
+import gym
+import random
+import numpy as np
+import time
+from collections import deque
+import pickle
+
+
+from collections import defaultdict
+
+
+EPISODES =   20000
+LEARNING_RATE = .1
+DISCOUNT_FACTOR = .99
+EPSILON = 1
+EPSILON_DECAY = .999
+
+
+
+def default_Q_value():
+    return 0
+
+
+if __name__ == "__main__":
+
+
+
+    random.seed(1)
+    np.random.seed(1)
+    env = gym.envs.make("FrozenLake-v0")
+    env.seed(1)
+    env.action_space.np_random.seed(1)
+
+
+    Q_table = defaultdict(default_Q_value) # starts with a pessimistic estimate of zero reward for each state.
+
+    episode_reward_record = deque(maxlen=100)
+
+
+    for i in range(EPISODES):
+        episode_reward = 0
+        obs = env.reset()
+        done = False
+
+        
+        #TODO PERFORM Q LEARNING
+        while done == False:
+            if random.uniform(0,1) < EPSILON:
+                action = env.action_space.sample()
+            else:
+                prediction = np.array([Q_table[(obs,i)] for i in range(env.action_space.n)])
+                action = np.argmax(prediction)
+                
+            next_obs,reward,done,info = env.step(action)
+            episode_reward += reward
+            if done:
+                Q_table[(obs, action)] = Q_table[(obs, action)] + LEARNING_RATE * (reward - Q_table[(obs,action)])
+            else:
+                prediction = np.array([Q_table[(next_obs,i)] for i in range(env.action_space.n)])
+                expect_reward = np.max(prediction)
+                Q_table[(obs, action)] = Q_table[(obs, action)] + LEARNING_RATE * (reward + DISCOUNT_FACTOR * expect_reward - Q_table[(obs, action)])
+                
+            obs = next_obs
+        
+        episode_reward_record.append(episode_reward)
+        
+        if i%100 ==0 and i>0:
+            print("LAST 100 EPISODE AVERAGE REWARD: " + str(sum(list(episode_reward_record))/100))
+            print("EPSILON: " + str(EPSILON) )
+        EPSILON = EPSILON * EPSILON_DECAY
+
+    ####DO NOT MODIFY######
+    model_file = open('Q_TABLE.pkl' ,'wb')
+    pickle.dump([Q_table,EPSILON],model_file)
+    #######################
+
+
+
+
+
+
+
